d2275b29d4ae0df3ac7f83a1a2ef2bfe817790e4
Nikita <kabanov.n.v@mail.ru>
2022-03-23 20:29:46 +0300

    добавлен gh-pages

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,22 @@
 function Footer() {
     return (
-        <footer className="page-footer blue accent-3">
-            <div className="footer-copyright">
-                <div className="container">
-                    <span>© {new Date().getFullYear()} React Movies Project</span>
-                    <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+        <footer className='page-footer blue accent-3'>
+            <div className='footer-copyright'>
+                <div className='container'>
+                    <span>
+                        © {new Date().getFullYear()} React Movies Project
+                    </span>
+                    <a
+                        className='grey-text text-lighten-4 right'
+                        href='https://KabanovN.github.io/react-movies'
+                        target='_blank'
+                        rel='noreferrer'>
+                        Repo
+                    </a>
                 </div>
             </div>
         </footer>
-    )
+    );
 }
 
 export default Footer;
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,23 @@
 function Header() {
     return (
-        <nav className="blue darken-1">
-            <div className="nav-wrapper">
-                <a href="#!" className="brand-logo">React Movies</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#!">Repo</a></li>
+        <nav className='blue darken-1'>
+            <div className='nav-wrapper'>
+                <a href='/' className='brand-logo'>
+                    React Movies
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a
+                            href='https://KabanovN.github.io/react-movies'
+                            target='_blank'
+                            rel='noreferrer'>
+                            Repo
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Header;
